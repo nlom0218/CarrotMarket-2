f1c0eb7bb10064f7ee1c1980a0c59ec5aa271f7f
nlom0218 <nlom0218@naver.com>
2024-03-23 23:50:03 +0900

    chore: tailwind 조건부 스타일 변경
    hover, dark 모드와 같이 특정 조건일 때의 스타일 변경

--- components/CardPractice/index.tsx
+++ components/CardPractice/index.tsx
@@ -1,28 +1,30 @@
 const CardPractice = () => {
   return (
-    <div className="w-full shadow-lg p-5 rounded-2xl">
+    <div className="bg-white w-full shadow-lg p-5 rounded-3xl max-w-screen-sm dark:bg-gray-600">
       <div className="flex justify-between items-center">
         <div className="flex flex-col">
-          <span className="text-gray-600 font-semibold -mb-1">In transit</span>
+          <span className="text-gray-600 font-semibold -mb-1 dark:text-gray-300">
+            In transit
+          </span>
           <span className="text-4xl font-semibold">Coolblue</span>
         </div>
         <div className="size-12 rounded-full bg-orange-400" />
       </div>
       <div className="my-2 flex items-center gap-2">
-        <span className="bg-green-400 text-white uppercase px-2.5 py-1.5 text-xs font-medium rounded-full">
+        <span className="bg-green-400 text-white uppercase px-2.5 py-1.5 text-xs font-medium rounded-full hover:bg-green-500 hover:scale-105 transition cursor-pointer">
           Today
         </span>
-        <span>9:30-10:30</span>
+        <span className="dark:text-gray-300">9:30-10:30</span>
       </div>
       <div className="relative">
         <div className="absolute bg-gray-200 rounded-full w-full h-2" />
         <div className="absolute bg-green-400 rounded-full w-2/3 h-2" />
       </div>
-      <div className="flex justify-between items-start mt-5 text-gray-600">
+      <div className="flex justify-between items-start mt-5 text-gray-600 dark:text-gray-300">
         <span>Expected</span>
         <span>Sorting center</span>
         <span>In transit</span>
-        <span className="text-gray-400">Delivered</span>
+        <span className="text-gray-400  dark:text-gray-500">Delivered</span>
       </div>
     </div>
   );
