84c89e69e40de3263dcdc759c7b7a249173f26bf
nlom0218 <nlom0218@naver.com>
2024-04-08 22:32:39 +0900

    feat: Server action으로 form 다루기

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,21 +3,36 @@ import FormInput from '@/components/FromInput';
 import SocialLogin from '@/components/SocialLogin';
 
 export default function Login() {
+  const handleForm = async (data: FormData) => {
+    'use server';
+    console.log(data.get('email'), data.get('password'));
+    console.log('i run in the server');
+  };
+
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
       <div className="flex flex-col gap-2 *:font-medium">
         <h1 className="text-2xl">안녕하세요!</h1>
         <h2 className="text-xl">Log in with email and password.</h2>
       </div>
-      <form className="flex flex-col gap-3">
-        <FormInput required type="email" placeholder="Email" errors={[]} />
+      <form className="flex flex-col gap-3" action={handleForm}>
+        <FormInput
+          required
+          type="email"
+          name="email"
+          placeholder="Email"
+          errors={[]}
+        />
         <FormInput
           required
           type="password"
+          name="password"
           placeholder="Password"
           errors={[]}
         />
-        <FromButton loading={false}>Log in</FromButton>
+        <FromButton type="submit" loading={false}>
+          Log in
+        </FromButton>
       </form>
       <SocialLogin />
     </div>
