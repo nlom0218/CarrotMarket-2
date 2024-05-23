c1f949ccd4eab8de52813af8cfc86749e9709c87
nlom0218 <nlom0218@naver.com>
2024-05-24 00:48:06 +0900

    feat: 이메일 로그인 기능 구현

--- app/login/actions.ts
+++ app/login/actions.ts
@@ -1,14 +1,31 @@
 'use server';
 
 import { z } from 'zod';
+import bcrypt from 'bcrypt';
 import {
   PASSWORD_MIN_LENGTH,
   PASSWORD_REGEX,
   PASSWORD_REGEX_ERROR,
 } from '@/libs/constants';
+import db from '@/libs/db';
+import getSession from '@/libs/session';
+import { redirect } from 'next/navigation';
+
+const existEmail = async (email: string) => {
+  const user = await db.user.findUnique({
+    where: {
+      email,
+    },
+    select: {
+      id: true,
+    },
+  });
+
+  return user;
+};
 
 const objectSchema = z.object({
-  email: z.string().email(),
+  email: z.string().email().refine(existEmail, '이메일이 존재하지 않습니다.'),
   password: z
     .string()
     .min(PASSWORD_MIN_LENGTH)
@@ -21,8 +38,41 @@ export const login = async (prevState: any, formData: FormData) => {
     password: formData.get('password'),
   };
 
-  const result = objectSchema.safeParse(data);
+  const result = await objectSchema.safeParseAsync(data);
 
-  if (!result.success) return result.error.flatten();
-  else console.log(result.data);
+  if (!result.success) {
+    return result.error.flatten();
+  }
+
+  const user = await db.user.findUnique({
+    where: {
+      email: result.data.email,
+    },
+    select: {
+      id: true,
+      password: true,
+    },
+  });
+
+  const comparePasswordResult = await bcrypt.compare(
+    result.data.password,
+    user!.password ?? ''
+  );
+
+  console.log(comparePasswordResult);
+
+  if (!comparePasswordResult) {
+    return {
+      fieldErrors: {
+        password: ['비밀번호가 틀립니다.'],
+        email: [],
+      },
+    };
+  }
+  const session = await getSession();
+  session.id = user!.id;
+
+  await session.save();
+
+  redirect('/profile');
 };
