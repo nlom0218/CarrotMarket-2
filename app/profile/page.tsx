93f9068ea3a1c0f3bba10cddf43ca63b55c91e03
nlom0218 <nlom0218@naver.com>
2024-05-24 22:02:58 +0900

    feat: profile에 로그아웃 기능 구현

---
+++ app/profile/page.tsx
@@ -0,0 +1,43 @@
+import db from '@/libs/db';
+import getSession from '@/libs/session';
+import { notFound, redirect } from 'next/navigation';
+
+async function getUser() {
+  const session = await getSession();
+
+  if (session.id) {
+    const user = await db.user.findUnique({
+      where: {
+        id: session.id,
+      },
+    });
+
+    if (user) {
+      return user;
+    }
+  }
+
+  notFound();
+}
+
+export default async function Profile() {
+  const user = await getUser();
+
+  const logOut = async () => {
+    'use server';
+
+    const session = await getSession();
+    session.destroy();
+
+    redirect('/');
+  };
+
+  return (
+    <div>
+      <h1>{user?.username}님, 환영합니다!</h1>
+      <form action={logOut}>
+        <button>로그아웃</button>
+      </form>
+    </div>
+  );
+}
