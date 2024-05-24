3fa1aaac40e3f50db92c5245277fd8b8ca630090
nlom0218 <nlom0218@naver.com>
2024-05-25 01:26:43 +0900

    feat: Middleware 도입

---
+++ middleware.ts
@@ -0,0 +1,12 @@
+import { cookies } from 'next/headers';
+import { NextRequest } from 'next/server';
+import getSession from './libs/session';
+
+export async function middleware(request: NextRequest) {
+  const session = await getSession();
+  console.log(session);
+
+  if (request.nextUrl.pathname === '/profile') {
+    return Response.redirect(new URL('/', request.url));
+  }
+}
