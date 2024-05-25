9ba13aa86929ed168f537d8681fdb551f1770bf9
nlom0218 <nlom0218@naver.com>
2024-05-25 12:13:21 +0900

    feat: 유저 인증 관련 미들웨어 추가

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,25 @@
-import { NextRequest } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
+import getSession from './libs/session';
 
-export async function middleware(request: NextRequest) {}
+const publicOnlyUrls: Record<string, boolean> = {
+  '/': true,
+  '/login': true,
+  '/sms': true,
+  '/create-account': true,
+};
+
+export async function middleware(request: NextRequest) {
+  const session = await getSession();
+  const isPublicUrl = publicOnlyUrls[request.nextUrl.pathname];
+
+  if (!session.id && !isPublicUrl) {
+    return NextResponse.redirect(new URL('/', request.url));
+  }
+
+  if (session.id && isPublicUrl) {
+    return NextResponse.redirect(new URL('/home', request.url));
+  }
+}
 
 export const config = {
   matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
