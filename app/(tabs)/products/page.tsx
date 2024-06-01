29e11fda0befc06180d53cbdca968815584d5a94
nlom0218 <nlom0218@naver.com>
2024-06-02 01:27:38 +0900

    feat: 상품 컴포넌트 구현 및 적용

--- app/(tabs)/products/page.tsx
+++ app/(tabs)/products/page.tsx
@@ -1,13 +1,26 @@
+import Product from '@/components/Product';
+import db from '@/libs/db';
+
 async function getProducts() {
-  await new Promise((res) => setTimeout(res, 100000));
+  return await db.product.findMany({
+    select: {
+      title: true,
+      price: true,
+      created_at: true,
+      photo: true,
+      id: true,
+    },
+  });
 }
 
 export default async function Products() {
   const products = await getProducts();
 
   return (
-    <div>
-      <h1 className="text-white text-4xl">Products!</h1>
+    <div className="p-5 flex flex-col gap-5">
+      {products.map((product) => (
+        <Product key={product.id} product={product} />
+      ))}
     </div>
   );
 }
---
+++ components/Product/index.tsx
@@ -0,0 +1,31 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+type Props = {
+  product: {
+    id: number;
+    created_at: Date;
+    title: string;
+    price: number;
+    photo: string;
+  };
+};
+
+export default function Product({ product }: Props) {
+  return (
+    <Link href={`/products/${product.id}`} className="flex gap-5">
+      <div className="relative min-h-28 min-w-28 rounded-md overflow-hidden">
+        <Image fill src={product.photo} alt={product.title} />
+      </div>
+      <div className="flex flex-col gap-1">
+        <span className="text-lg text-white">{product.title}</span>
+        <span className="text-sm text-neutral-500">
+          {product.created_at.toString()}
+        </span>
+        <span className="text-lg text-white font-semibold">
+          {product.price.toLocaleString()}원
+        </span>
+      </div>
+    </Link>
+  );
+}
