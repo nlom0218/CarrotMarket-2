d423bef138498865cf3b315710ee0754c9b3756d
nlom0218 <nlom0218@naver.com>
2024-06-02 00:28:03 +0900

    feat: 상품 Skeleton 구현

--- app/(tabs)/products/page.tsx
+++ app/(tabs)/products/page.tsx
@@ -1,7 +1,13 @@
-export default function Product() {
+async function getProducts() {
+  await new Promise((res) => setTimeout(res, 100000));
+}
+
+export default async function Products() {
+  const products = await getProducts();
+
   return (
     <div>
-      <h1 className="text-white text-4xl">Product!</h1>
+      <h1 className="text-white text-4xl">Products!</h1>
     </div>
   );
 }
